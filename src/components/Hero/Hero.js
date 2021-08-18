97742ac3efc2c35c8dd308b6c28aac26ad496dbe
William Jones <37749088+wdj82@users.noreply.github.com>
2021-08-18 15:04:06 -0500

    solutions

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -4,8 +4,26 @@ import styled from 'styled-components/macro';
 const Hero = () => {
   return (
     <Wrapper>
-      <HeroImage src="/images/hero-img.jpg" />
-      <Swoop src="/swoop.svg" />
+      <picture>
+        <source
+          type='image/avif'
+          srcSet={`
+              /images/hero-img.avif 1x,
+              /images/hero-img@2x.avif 2x,
+              /images/hero-img@3x.avif 3x,
+            `}
+        />
+        <source
+          type='image/jpeg'
+          srcSet={`
+              /images/hero-img.jpg 1x,
+              /images/hero-img.jpg@2x 2x,
+              /images/hero-img.jpg@3x 3x,
+            `}
+        />
+        <HeroImage alt='' src='/images/hero-img.jpg' />
+      </picture>
+      <Swoop alt='' src='/swoop.svg' />
     </Wrapper>
   );
 };
